f25d6a3e92bf38054ea1c9283d055ae38778cf48
ApexioDaCoder <apexiodacoder@gmail.com>
2021-10-13 21:24:07 +0000

    Delete PostLink

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import matter from "gray-matter";
 import Link from "../components/Link";
 import path from "path";
 import Layout, { Section } from "../components/Layout";
-import PostLink from "../components/PostLink";
 import Head from "next/head";
 import { postFilePaths, POSTS_PATH } from "../utils/mdxUtils";
 import styled, { keyframes } from "styled-components";
@@ -32,34 +31,75 @@ const bounce = keyframes`
   0%, to {
       transform: translateY(-25%) translateX(-50%);
       -webkit-animation-timing-function: cubic-bezier(.8,0,1,1);
-      animation-timing-function: cubic-bezier(.8,0,1,1)
+      animation-timing-function: cubic-bezier(.8,0,1,1);
   }
 
   50% {
       transform: translateX(-50%);
       -webkit-animation-timing-function: cubic-bezier(0,0,.2,1);
-      animation-timing-function: cubic-bezier(0,0,.2,1)
+      animation-timing-function: cubic-bezier(0,0,.2,1);
   }
 `;
 
 const ScrollReminder = styled.p`
+  --y-offset: 0px;
+  --opacity: 1;
   display: block;
   padding: 10px;
   position: fixed;
   transform: translateX(-50%);
-  top: calc(100% - 100px);
+  bottom: calc(0% - var(--y-offset));
   left: 50%;
+  opacity: var(--opacity);
   background-color: rgba(110, 118, 129, 0.4);
   border-radius: 10px;
   animation: ${bounce} 1s infinite;
+  transition: all 0.3s cubic-bezier(0, 0, 0.2, 1);
 `;
 
 export default function Index({ posts }) {
+  const atTop = useAtTop();
+
   return (
     <Layout>
       <Head>
         <title>Blog</title>
       </Head>
+      <ScrollReminder
+        style={{ "--y-offset": atTop ? "0px" : "100px", "--opacity": atTop ? 1 : 0 }}
+      >
+        <Icon
+          aria-hidden="true"
+          focusable="false"
+          data-prefix="fas"
+          data-icon="angles-down"
+          className="svg-inline--fa fa-angles-down"
+          role="img"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 384 512"
+        >
+          <path
+            fill="currentColor"
+            d="M169.4 278.6C175.6 284.9 183.8 288 192 288s16.38-3.125 22.62-9.375l160-160c12.5-12.5 12.5-32.75 0-45.25s-32.75-12.5-45.25 0L192 210.8L54.63 73.38c-12.5-12.5-32.75-12.5-45.25 0s-12.5 32.75 0 45.25L169.4 278.6zM329.4 265.4L192 402.8L54.63 265.4c-12.5-12.5-32.75-12.5-45.25 0s-12.5 32.75 0 45.25l160 160C175.6 476.9 183.8 480 192 480s16.38-3.125 22.62-9.375l160-160c12.5-12.5 12.5-32.75 0-45.25S341.9 252.9 329.4 265.4z"
+          ></path>
+        </Icon>{" "}
+        Scroll Down{" "}
+        <Icon
+          aria-hidden="true"
+          focusable="false"
+          data-prefix="fas"
+          data-icon="angles-down"
+          className="svg-inline--fa fa-angles-down"
+          role="img"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 384 512"
+        >
+          <path
+            fill="currentColor"
+            d="M169.4 278.6C175.6 284.9 183.8 288 192 288s16.38-3.125 22.62-9.375l160-160c12.5-12.5 12.5-32.75 0-45.25s-32.75-12.5-45.25 0L192 210.8L54.63 73.38c-12.5-12.5-32.75-12.5-45.25 0s-12.5 32.75 0 45.25L169.4 278.6zM329.4 265.4L192 402.8L54.63 265.4c-12.5-12.5-32.75-12.5-45.25 0s-12.5 32.75 0 45.25l160 160C175.6 476.9 183.8 480 192 480s16.38-3.125 22.62-9.375l160-160c12.5-12.5 12.5-32.75 0-45.25S341.9 252.9 329.4 265.4z"
+          ></path>
+        </Icon>
+      </ScrollReminder>
       <Section style={{ minHeight: "100vh" }}>
         <Grid>
           <div>
@@ -77,39 +117,6 @@ export default function Index({ posts }) {
           </div>
           <PostList posts={posts} />
         </Grid>
-        <ScrollReminder>
-          <Icon
-            aria-hidden="true"
-            focusable="false"
-            data-prefix="fas"
-            data-icon="angles-down"
-            className="svg-inline--fa fa-angles-down"
-            role="img"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 384 512"
-          >
-            <path
-              fill="currentColor"
-              d="M169.4 278.6C175.6 284.9 183.8 288 192 288s16.38-3.125 22.62-9.375l160-160c12.5-12.5 12.5-32.75 0-45.25s-32.75-12.5-45.25 0L192 210.8L54.63 73.38c-12.5-12.5-32.75-12.5-45.25 0s-12.5 32.75 0 45.25L169.4 278.6zM329.4 265.4L192 402.8L54.63 265.4c-12.5-12.5-32.75-12.5-45.25 0s-12.5 32.75 0 45.25l160 160C175.6 476.9 183.8 480 192 480s16.38-3.125 22.62-9.375l160-160c12.5-12.5 12.5-32.75 0-45.25S341.9 252.9 329.4 265.4z"
-            ></path>
-          </Icon>{" "}
-          Scroll Down{" "}
-          <Icon
-            aria-hidden="true"
-            focusable="false"
-            data-prefix="fas"
-            data-icon="angles-down"
-            className="svg-inline--fa fa-angles-down"
-            role="img"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 384 512"
-          >
-            <path
-              fill="currentColor"
-              d="M169.4 278.6C175.6 284.9 183.8 288 192 288s16.38-3.125 22.62-9.375l160-160c12.5-12.5 12.5-32.75 0-45.25s-32.75-12.5-45.25 0L192 210.8L54.63 73.38c-12.5-12.5-32.75-12.5-45.25 0s-12.5 32.75 0 45.25L169.4 278.6zM329.4 265.4L192 402.8L54.63 265.4c-12.5-12.5-32.75-12.5-45.25 0s-12.5 32.75 0 45.25l160 160C175.6 476.9 183.8 480 192 480s16.38-3.125 22.62-9.375l160-160c12.5-12.5 12.5-32.75 0-45.25S341.9 252.9 329.4 265.4z"
-            ></path>
-          </Icon>
-        </ScrollReminder>
       </Section>
       <Section>
         <h2>About Me</h2>
